b3231590d49ab061042cdc23ad421ddd57121136
Leandro Souza <leandrosouzaf30@outlook.com>
2024-05-23 21:45:00 -0300

    (feat): Adiciona componente search e busca inicial por issue

--- src/pages/Profile/components/BuscaPublicacoes/index.tsx
+++ src/pages/Profile/components/BuscaPublicacoes/index.tsx
@@ -1,7 +1,49 @@
+import { SearchFormContainer } from "./styles";
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from "react";
+import { api } from "../../../../lib/axios";
+
+// Mapeia o tipo de dado a ser retornado
+const searchFormSchema = z.object({
+    query: z.string(),
+  });
+  
+  // Cria tipagem do campo de busca
+  type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
+
 export function BuscaPublicacoes(){
+    const [searchIssue, SetsearcheIssue] = useState([])
+
+       
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+      } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    });
+
+    async function handleSearchTransactions(data: SearchFormInputs){
+        const owner = "leandrosouzaf30"
+        const repo = "github-blog"
+        const fullQuery = `${data.query} repo:${owner}/${repo}`;
+  
+        const response = await api.get('search/issues', {
+            params: {
+              q: fullQuery
+            }
+          })
+      
+          SetsearcheIssue(response.data)
+          console.log(searchIssue)
+    }
+
     return(
-        <div>
-            <h1>Busca Pubublicações</h1>
-        </div>
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input type="text" placeholder="Busque por transações" {...register('query')}/>
+        </SearchFormContainer>          
     )
 }
